0304e14819a914925706d5b220a41a454e117fbe
brandiw <brandimichelle.0911@gmail.com>
2018-08-23 11:25:30 -0700

    Bare bones express app done

---
+++ index.js
@@ -0,0 +1,22 @@
+// Require needed modules
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+
+// Declare app variable
+var app = express();
+
+// Set and use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Define routes
+app.get('/', function(req, res){
+  res.send('hi from home route');
+});
+
+// Listen on port 3000
+app.listen(3000);
