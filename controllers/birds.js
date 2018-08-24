27f1539abde88cf52d36d8fb996a51eb14d38149
yuanjli <liyuanjienetwork@gmail.com>
2018-08-24 10:28:00 -0700

    Added bird view

--- controllers/birds.js
+++ controllers/birds.js
@@ -4,12 +4,27 @@ var express = require('express');
 // Declare a new router
 var router = express.Router();
 
+
+// Require the database
+var db = require('../models');
+
 // Get the authorization helper function
 var loggedIn = require('../middleware/loggedIn');
 
 // Define routes
 router.get('/', loggedIn, function(req, res){
-  res.render('birds/index');
+	db.bird.findAll({
+		where: { userId: req.user.id }
+	}).then(function(birds){ 
+		res.render('birds/index', { birds: birds });
+	}).catch(function(err){
+		console.log(err);
+		res.send('oops');
+	});
+  // res.render('birds/index');
 });
+
+
+
 
 module.exports = router;
